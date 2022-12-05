fbebacb2be828ad0c8db476f48efb6ececd529be
dell Precision <bkar.mrwan16@gmail.com>
2022-12-05 11:57:53 +0100

    admin panel

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Post,Profile,Image
+from django.contrib.auth.models import Group
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.unregister(Group)
